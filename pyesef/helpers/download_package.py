4057f9f72e14d3d7dd86283e3dae3f27557ad045
Gregorio MONGELLI (ACSONE) <gregorio.mongelli@acsone.eu>
2024-01-03 11:57:53 +0100

    [#72924][GMO][CHG]change the way a report is listed and downloaded

--- pyesef/helpers/download_package.py
+++ pyesef/helpers/download_package.py
@@ -3,7 +3,9 @@ from dataclasses import dataclass
 import json
 import os
 from pathlib import Path
-import urllib.request
+from  urllib import request
+from urllib.parse import quote, unquote
+from urllib.error import HTTPError
 
 import requests
 
@@ -42,14 +44,8 @@ def _cleanup_package_dict(identifier_map: IdentifierType) -> list[Filing]:
     for key, _ in identifier_map.items():
         filing_list = identifier_map[key]
 
-        if len(filing_list) == 1:
-            data_list.append(filing_list[0])
-            continue
-
         for filing in filing_list:
-            if "en" in filing.file_name:
-                data_list.append(filing)
-                continue
+            data_list.append(filing)
 
     return data_list
 
@@ -63,27 +59,29 @@ def download_packages() -> None:
     identifier_map: IdentifierType = {}
     idx: int = 0
 
-    with urllib.request.urlopen(f"{BASE_URL}table-index.json") as url:
+    with request.urlopen(f"{BASE_URL}api/filings?page[number]=0&page[size]=999999") as url:
         data = json.loads(url.read().decode())
-        for _, item in enumerate(data):
-            if item["country"] in [
-                Country.DENMARK,
-                Country.FINLAND,
-                Country.ICELAND,
-                Country.NORWAY,
-                Country.SWEDEN,
-            ]:
-                lei = item["lei"]
-                filing = Filing(
-                    country=_parse_file_ending(path=item["path"]),
-                    file_name=item["report-package"],
-                    path=item["path"],
-                )
+        for _, item in enumerate(data["data"]):
+            try:
+                if True:
+                    package_url = item["attributes"]["package_url"]
+                    if package_url is None:
+                        continue
+                    else:
+                        package_url = unquote(package_url)
+                    lei = os.path.basename(unquote(item["relationships"]["entity"]["links"]["related"]))
+                    filing = Filing(
+                        country=item["attributes"]["country"],
+                        file_name=os.path.basename(package_url),
+                        path=os.path.dirname(package_url),
+                    )
 
-                if lei not in identifier_map:
-                    identifier_map[lei] = [filing]
-                else:
-                    identifier_map[lei].append(filing)
+                    if lei not in identifier_map:
+                        identifier_map[lei] = [filing]
+                    else:
+                        identifier_map[lei].append(filing)
+            except KeyError as e:
+                print(f"KeyError: {e} for entry {item['id']}, it will be ignored")
 
     data_list = _cleanup_package_dict(identifier_map=identifier_map)
 
@@ -98,17 +96,20 @@ def download_packages() -> None:
 
 def _download_package(filing: Filing) -> None:
     """Download a package and store it the archive-folder."""
-    url = f"{BASE_URL}{filing.path}/{filing.file_name}"
+    url = f"{BASE_URL}{filing.path}/{quote(filing.file_name)}"
 
-    download_path = os.path.join(PATH_ARCHIVES, filing.country)
+    download_path = os.path.join(PATH_ARCHIVES, filing.path[1:])
 
     # Create download path if it does not exist
     Path(download_path).mkdir(parents=True, exist_ok=True)
 
     print(f"Downloading {url}")  # noqa: T201
 
-    req = requests.get(url, stream=True, timeout=30)
+
     write_location = os.path.join(download_path, filing.file_name)
-    with open(write_location, "wb") as _file:
-        for chunk in req.iter_content(chunk_size=2048):
-            _file.write(chunk)
+    if not os.path.exists(write_location):
+        try:
+            request.urlretrieve(url, write_location)
+        except HTTPError as e:
+            if e.code == 404:
+                print(f"Skipping download of {url}")
